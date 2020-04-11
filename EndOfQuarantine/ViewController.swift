9836684c8082716d4be921bb080935dc584bedeb
fairmentat <fairmentat@mail.ru>
2020-04-11 15:51:47 +0400

    First solution with LABEL

--- EndOfQuarantine/ViewController.swift
+++ EndOfQuarantine/ViewController.swift
@@ -12,9 +12,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        countEnd()
+        
+        
+        
+        
+        
     }
 
+    @IBOutlet weak var LableCount: UILabel!
+    
+    
+    func countEnd () {
+        
+        LableCount.text = "Dima"
+        
+        
+        
+    }
 
 }
+
 
